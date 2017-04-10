d277607a4ae1f0b5e3b082890aa54de9419d8e39
Sven Koschnicke <s.koschnicke@gfxpro.com>
2017-04-10 12:03:51 +0200

    Updated publish workflow.

---
+++ publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script upates the last update date in the header of the docs and commits
+# all changes, which triggers the publish script (post-commit-hook).
+
+# Update the date to the date of the last commit (which is the current date)
+formatted_date=$(date +%Y-%m-%d)
+sed -i "/[0-9]{4}-[0-9]{2}-[0-9]{2}$/c\\$formatted_date" index.adoc
+git add index.adoc
+git commit
